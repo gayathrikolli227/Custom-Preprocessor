c03227b56077db9e33503e1f43b0687c9116c589
gayathrikolli227 <gayathrikolli318@gmail.com>
2026-05-18 22:33:48 +0530

    Update inputfile.c

--- Sourcecode/inputfile.c
+++ Sourcecode/inputfile.c
@@ -1,16 +1,32 @@
+/* Header file inclusion */
 #include <myheader.h>
 
+/* Macro definitions */
 #define MESSAGE HELLO
 #define PI 3.14
+
+/* Main function */
 int main()
 {
-        int r=5; //this is radius
-        float area;
-        /*AREA FORMULA:
-         PI*r*r
-         */
+        /* Radius declaration */
+        int r=5;
 
-        area=PI*r*r;
-        printf(MSG);
+        // Single-line comment (for testing comment removal)
+
+        /* Variable to store calculated area */
+        float area;
+
+        /* Multi-line comment (for testing comment removal)
+           Area Formula:
+           PI * r * r
+        */
+
+        /* Area calculation using macro substitution */
+        area = PI * r * r;
+
+        /* Display message using macro replacement */
+        printf(MESSAGE);
+
+        /* Program termination */
         return 0;
 }
